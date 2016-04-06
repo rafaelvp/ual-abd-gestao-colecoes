67d0717aad503393b14dd3a4eb47e2965618a645
Rafael Victória-Pereira <vp.rafael@gmail.com>
2016-04-06 22:32:28 +0100

    Correção do urls para corrigir a view por defeito

--- GestaoColecoes/colecoes/urls.py
+++ GestaoColecoes/colecoes/urls.py
@@ -8,9 +8,9 @@ from django.conf.urls import patterns, url
 from colecoes import views
 
 urlpatterns = patterns('',
-    url(r'^hello_world$', views.hello_world, name='hello_world'),
-    url(r'^.*$', views.index, name='index'), #.* significa qualquer caracter, 0 ou mais vezes
+    url(r'^$', views.index, name='index'), #.* significa qualquer caracter, 0 ou mais vezes
     url(r'^fat_controller$', views.fat_controller, name='index_no_model'),
     url(r'^with_model$', views.model_controller, name='model_controller'),
     url(r'^MVC$', views.MVC_way, name='MVC_way'),
+    url(r'^hello_world$', views.hello_world, name='hello_world'),
 )
